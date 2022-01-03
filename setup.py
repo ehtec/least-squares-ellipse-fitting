eac71a59807bad0ff65e8d4b101f8d3df60af77f
Elias Hohl <elias.hohl@ehtec.co>
2022-01-03 02:58:35 +0100

    Prepare for pypi

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="lsq-ellipse",
-    version="3.0.2",
-    description="Lease squares fitting of Ellipses",
+    name="ellipsefitting",
+    version="1.0",
+    description="Least squares fitting of Ellipses",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/ehtec/least-squares-ellipse-fitting",
     author="Elias Hohl",  # forked from Ben Hammel
-    author_email="bdhammel@gmail.com",
+    author_email="elias.hohl@ehtec.co",
     license="MIT",
     py_modules=['ellipse'],
     install_requires=["numpy"],
